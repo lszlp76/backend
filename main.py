cd6e76daddd52d5ffbd8ac11680236af621bea71
ulasozalp <uozalp.uo@gmail.com>
2025-12-14 22:55:15 +0300

    main.py klasör hatası

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 
 # --- Kendi oluşturduğumupip z dosyalar ---
 import models
-from ruyatabiri.database import engine, SessionLocal
+from database import engine, SessionLocal
 
 # --- Ayarlar ---
 load_dotenv()
