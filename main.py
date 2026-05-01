33c7ad727a6a24b5c4dcc34c2e2d81303f976e8a
ulas <56877597+lszlp76@users.noreply.github.com>
2026-05-01 11:45:49 +0300

    Update Hugging Face API URL for image requests

--- main.py
+++ main.py
@@ -433,7 +433,9 @@ def analiz_et(istek: RuyaIstegi, db: Session = Depends(get_db)):
 
                 # b. Hugging Face API'ye İstek At (Stable Diffusion XL modeli)
                 print(f"🎨 Hugging Face'ten resim isteniyor. Prompt: {gorsel_prompt}")
-                hf_api_url = "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0"
+                # hf_api_url = "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0"
+                                 # yeni url
+                 hf_api_url = "https://router.huggingface.co/hf-inference/models/prompthero/openjourney"
                 headers = {"Authorization": f"Bearer {HF_API_KEY}"}
                 payload = {"inputs": gorsel_prompt}
 
